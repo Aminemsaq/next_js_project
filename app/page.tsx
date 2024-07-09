51b6814b1d2d85f43fdcd1bc55352d5ed930cb17
Msaq Amine <122275971+amineleet@users.noreply.github.com>
2024-07-09 15:14:59 +0100

    last final

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,25 @@
-import dynamic from 'next/dynamic';
+import Carrousels from '@/components/Carrousels';
+import Fab from '@/components/Fab';
+import Faq from '@/components/Faq';
+import Footer from '@/components/Footer';
+import Hero from '@/components/Hero';
+import Offers from '@/components/Offers';
+import Proof from '@/components/Proof';
 import React from 'react';
+import Head from 'next/head';
 
-const Landing = dynamic(() => import('@/components/Landing'));
+
 
 export default function Home() {
   return (
     <div>
-      <Landing />
+      <Hero />
+      <Proof />
+      <Fab />
+      <Carrousels />
+      <Offers />
+      <Faq />
+      <Footer />
     </div>
   );
 }

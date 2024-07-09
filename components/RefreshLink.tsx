6ecf6cebc295f8b8dd0d42d61d7edabe07913a8d
Msaq Amine <122275971+amineleet@users.noreply.github.com>
2024-07-09 11:14:16 +0100

    last last

--- components/RefreshLink.tsx
+++ components/RefreshLink.tsx
@@ -1,14 +1,20 @@
-// components/RefreshLink.js
-import React from 'react';
+// components/RefreshLink.tsx
+import React, { ReactNode } from 'react';
 
-const RefreshLink = ({ href, children }) => {
-  const handleClick = (e) => {
+interface RefreshLinkProps {
+  href: string;
+  children: ReactNode;
+  className?: string;
+}
+
+const RefreshLink: React.FC<RefreshLinkProps> = ({ href, children, className }) => {
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
     e.preventDefault();
     window.location.href = href;
   };
 
   return (
-    <a href={href} onClick={handleClick}>
+    <a href={href} onClick={handleClick} className={className}>
       {children}
     </a>
   );
